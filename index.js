41c5490544757478eac5245ae3cbd9239b10eded
Kushal Patel <kushalpatel0265@gmail.com>
2024-03-09 14:23:31 +0530

    change index.js file

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-// require('dotenv').config({path: './env'})
-import dotenv from "dotenv"
+import app from "./server.js";
+import {MongoClient} from "mongodb";
 import connectDB from "./connectdb.js";
-import app from './server.js'
-dotenv.config({
-    path: './.env'
-})
+import ReviewsDAO from "./reviews.DAO.js";
 
+const port = 9000;
 
+const url = "mongodb+srv://kushalpatel0265:Kushal2018@cluster0.b0nwiel.mongodb.net/"
+MongoClient.connect(url)
+    .then(client => {
+        const result =  ReviewsDAO.injectDB(client);
+        if(result){
+            console.log("true");
+        }else{
+            console.log("false");
+        }
+        app.listen(port, () => {
+            console.log(`listening on port ${port}`)
+        })
 
-connectDB()
-.then(() => {
-    app.listen(process.env.PORT || 9000, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
-})
-.catch((err) => {
-    console.log("MONGO db connection failed !!! ", err);
-})
+    .catch(err => {
+        console.error(err.stack);
+        process.exit(1);
+    });
