e130a71fc721fc87e677289596ca8ada73f90670
Kushal Patel <kushalpatel0265@gmail.com>
2024-03-17 18:36:48 +0530

    edit

--- server/reviews.route.js
+++ server/reviews.route.js
@@ -3,7 +3,7 @@ import ReviewsCtrl from "./reviews.controller.js"
 
 const router = express.Router()
 
-router.route("/movie/:id").get(ReviewsCtrl.apiGetReview)
+router.route("/movie/:id").get(ReviewsCtrl.apiGetReviews)
 router.route("/new").post(ReviewsCtrl.apiPostReview)
 router.route("/:id")
     .get(ReviewsCtrl.apiGetReview)
